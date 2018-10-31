f964d1219780068d146f072ee361e1065f12271a
dddddd999 <dddddd999@qq.com>
2018-10-31 11:57:47 +0800

    admin vip movie

---
+++ fuli/content/cache/sort.php
@@ -0,0 +1,1 @@
+<?php exit;//a:1:{i:1;a:9:{s:6:"lognum";s:1:"6";s:8:"sortname";s:6:"福利";s:11:"description";s:0:"";s:5:"alias";s:0:"";s:3:"sid";i:1;s:5:"taxis";i:0;s:3:"pid";i:0;s:8:"template";s:0:"";s:8:"children";a:0:{}}}
---
+++ fuli/content/templates/default/header.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Template Name:默认模板
+Description:默认模板，简洁优雅
+Version:1.2
+Author:emlog
+Author Url:http://www.emlog.net
+Sidebar Amount:1
+*/
+if(!defined('EMLOG_ROOT')) {exit('error!');}
+require_once View::getView('module');
+?>
